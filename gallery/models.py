fb12364625db13e9fcee0383c86451035e68cd5c
Michael Schwarz <hallo@michaelschwarz.digital>
2016-08-18 13:08:25 +0200

    add image and post to models.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+import uuid
 
 class Publisher(models.Model):
 
 class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+    description = models.CharField(max_length=500)
+    reason = models.CharField(max_length=500)
 
-class Image(models.Model): abc test
+class Image(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    year = models.DateTimeField('year published')
+    location = models.CharField(max_length=50)
+    url = models.URLField(max_length=200)
