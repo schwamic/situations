cdc5f252dd3998c62fdf2d13c160f5771d603061
Michael Schwarz <hallo@michaelschwarz.digital>
2016-08-18 13:24:48 +0200

    makemigrations

--- gallery/models.py
+++ gallery/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import uuid
 
 class Publisher(models.Model):
+    pass
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
