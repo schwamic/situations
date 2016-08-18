ab510a1d7a37d3ba292e9351a0f270b799a72922
Michael Schwarz <hallo@michaelschwarz.digital>
2016-08-18 13:46:01 +0200

    admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Publisher
+from .models import Image
+from .models import Post
+
+
+admin.site.register(Publisher)
+admin.site.register(Image)
+admin.site.register(Post)
