fafe197a7735fec21b31f1ac60b929def7dade03
Michael Schwarz <hallo@michaelschwarz.digital>
2016-08-19 11:35:52 +0200

    ImageView, situations.urls

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Image
 
 # Create your views here.
+class imageView(generic.ListView):
+    model = Image
+    template_name = 'gallery/images.html'
