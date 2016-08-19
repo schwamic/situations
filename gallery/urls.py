15448aff49d9af2ac69f5d1bb6b4c183eaf1b3a6
Sebastian <s.oley@me.com>
2016-08-19 12:10:04 +0200

    urls, map view, base html

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'gallery'
 urlpatterns = [
     url(r'^$', views.PostsView.as_view(), name='posts'),
-    url(r'^images/$', views.ImagesView.as_view(), name='images'),
+    url(r'^/images/$', views.ImagesView.as_view(), name='images'),
+    url(r'^/map/$', views.MapView.as_view(), name='map'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.views import generic
 from .models import Publisher, Image, Post
-
 
 class ImagesView(generic.ListView):
     model = Image
     template_name = 'gallery/images.html'
+
+class MapView(generic.ListView):
+    model = Post
+    template_name = 'gallery/map.html'
 
 class PostsView(generic.ListView):
     model = Post
