f4eba6be9ee28a5551270c2f81c5364d92a401c7
Michael Schwarz <hallo@michaelschwarz.digital>
2016-08-19 15:57:05 +0200

    media url.py settings.py db

--- situations/settings.py
+++ situations/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR)+'/situations', 'media')
