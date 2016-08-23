726b0197dd69fc52a6478f3a40bce349b232643b
Michael Schwarz <hallo@michaelschwarz.digital>
2016-08-23 10:31:00 +0200

    frontend update

--- templates/static/js/components/lightbox.js
+++ templates/static/js/components/lightbox.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	$('.lightbox').click(function(event){
-		$('.backdrop, .box').animate({'opacity':'.50'}, 300, 'linear');
+		$('.backdrop, .box').animate({'opacity':'1.00'}, 300, 'linear');
 		$('.box').animate({'opacity':'1.00'}, 300, 'linear');
 		$('.backdrop, .box').css('display', 'block');
 		thumb_id = (""+$(this).attr("id")).split("_")[1];
@@ -17,11 +17,6 @@ $(document).ready(function(){
 	$('.close').click(function(){
 		close_box();
 	});
-
-	$('.backdrop').click(function(){
-		close_box();
-	});
-
 });
 
 function close_box()
