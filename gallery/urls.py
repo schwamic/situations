ef91d9c2e9f00aa29c49a1f114170a4687c0b2a1
Sebastian <s.oley@me.com>
2016-08-24 15:17:04 +0200

    fix

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,10 +7,9 @@ urlpatterns = [
     url(r'^images/$', views.ImagesView.as_view(), name='images'),
     url(r'^posts/$', views.PostsView.as_view(), name='posts'),
     url(r'^map/$', views.MapView.as_view(), name='map'),
-    url(r'^images/(?P<id>[^/]+)/publish/$', views.publish, name='publish'),
-    url(r'^images/(?P<id>[^/]+)/$', views.ThankYouView.as_view(), name='thankyou'),
+    url(r'^images/(?P<publisher_id>[0-9]+)/publish/$', views.publish, name='publish'),
+    url(r'^images/(?P<pk>[[0-9]+)/thankyou/$', views.ThankYouView.as_view(), name='thankyou'),
 ]
-
 # note
 # uuid: (?P<something>[^/]+)
 # number: (?P<something>[0-9]+)
