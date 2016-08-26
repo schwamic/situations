a6ade3bfb0ff17caa174d92715c679ad4596fcb7
Michael Schwarz <hallo@michaelschwarz.digital>
2016-08-26 12:29:10 +0200

    load images

--- templates/static/js/components/thumbnail.js
+++ templates/static/js/components/thumbnail.js
@@ -1,5 +1,13 @@
-$('.thumbnail').hover(function(){
-    $(this).find('.caption').css('opacity','1');
-}, function(){
-    $(this).find('.caption').css('opacity','0');
+$(document).ready(function() {
+    $('#content').imagesLoaded( function() {
+    console.log('#container background image loaded');
+    $('img').fadeIn(1000);
+    });
+
+    $('.thumbnail').hover(function(){
+        $(this).find('.caption').css('opacity','1');
+        }, function(){
+            $(this).find('.caption').css('opacity','0');
+    });
 });
+
