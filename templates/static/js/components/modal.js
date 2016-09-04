23eb6a3e0ed3d394ac40d3a1bae86e38a4bdfb59
Michael Schwarz <hallo@michaelschwarz.digital>
2016-09-04 14:43:46 +0200

    cookie

--- templates/static/js/components/modal.js
+++ templates/static/js/components/modal.js
@@ -1,8 +1,12 @@
 "use strict";
 $(document).ready(function(){
+
      console.log('cookie: '+$.cookie('pop'));
      if ($.cookie('pop') != 'checked') {
       $('#welcomeModal').modal('show');
-      $.cookie('pop', 'checked', { expires: 7 });
     }
+
+    $('.modal_btn').click(function(){
+        $.cookie('pop', 'checked', { expires: 7 });
+    });
 });
