ca13f0ceb77e1d70b059ef882738c9ab9ec684e8
Michael Schwarz <hallo@michaelschwarz.digital>
2016-09-06 15:47:37 +0200

    update

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 app_name = 'gallery'
 urlpatterns = [
-    url(r'^$', lambda request: HttpResponsePermanentRedirect('/posts/')),
+    url(r'^$', views.PostsView.as_view(), name='posts'),
     url(r'^images/$', views.ImagesView.as_view(), name='images'),
     url(r'^posts/$', views.PostsView.as_view(), name='posts'),
     url(r'^map/$', views.MapView.as_view(), name='map'),
